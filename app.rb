f77d2efae45479d62fe81702ca59c7b3af72fbd3
david hsu <chawei.hsu@gmail.com>
2012-02-14 16:09:49 -0800

    cleanup

--- app.rb
+++ app.rb
@@ -11,17 +11,6 @@ class App < Sinatra::Base
 
   put '/' do
     temp_file = params["tweet_file"][:tempfile]
-    temp_file = File.open(temp_file.path)
-    data = temp_file.read
-
-    timestamp = Time.now.to_i
-    filename  = "#{timestamp}-#{params["tweet_file"][:filename]}"
-    p "root: #{settings.root}"
-    File.open("/app/tmp/#{filename}", "w") do |f|
-      f.write(data)
-    end
-
-    p params["tweet_caption"]
    
     content_type :json
     { :temp_filename => temp_file.path }.to_json
@@ -31,11 +20,9 @@ class App < Sinatra::Base
     filename = params["image_filename"]
     caption  = params["caption"]
 
-    p filename
-    p caption
+    p "path: #{filename}"
+    p "caption: #{caption}"
     f = File.open("#{filename}")
-    #data = f.read
-    #p data
     p Twitter.update_with_media(caption, f)
   end
 end
